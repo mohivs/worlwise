74e904696af7010b11ae088f9380ca0142db678c
mohsen <mohisls2012@gmail.com>
2024-06-18 20:49:04 +0330

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>hello</p>} />
+          <Route path="countries" element={<p>list of countries</p>} />
+          <Route path="form" element={<p>form</p>} />
+        </Route>
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,16 +1,22 @@
 import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import styles from "./PageNav.module.css";
+
 function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/Pricing">pricing</NavLink>
         </li>
         <li>
           <NavLink to="/Product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            login
+          </NavLink>
         </li>
       </ul>
     </nav>
